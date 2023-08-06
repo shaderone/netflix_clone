7b3a60405916545b52b75493112803e5606ffba5
pixiedust07 <sharonshaji127001@gmail.com>
2023-08-06 14:19:46 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/shaderone/netflix_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shaderone.netflix_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NetflixApp());
+}
+
+class NetflixApp extends StatelessWidget {
+  const NetflixApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(child: Text("start")),
+    );
+  }
+}
